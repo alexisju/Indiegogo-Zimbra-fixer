638488df62d9bc5100521fc256a19f147d2208c9
Alexis J <alexis@effingo.be>
2017-03-23 14:37:11 +0100

    Create indiegogo-zimbra-fixer.js

---
+++ indiegogo-zimbra-fixer.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Indiegogo zimbra fixer
+// @description A simple script to fix images display of Indiegogo Newsletter on Zimbra webmail.
+// @author			alexisju
+// @namespace   webmail.web4all.fr
+// @include     https://webmail.web4all.fr/*
+// @version     1
+// @grant       none
+// @run-at			document-end
+// @license			GPL v3 or later version
+// @downloadURL		https://raw.githubusercontent.com/alexisju/indiegogo-zimbra-fixer/master/indiegogo-zimbra-fixer.js
+// ==/UserScript==
+
+
+document.body.innerHTML = document.body.innerHTML.replace('pnsrc','src');
